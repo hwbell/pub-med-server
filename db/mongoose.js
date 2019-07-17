e29110b897f8d5352a30b3c991733cff71b20d32
Harry <harold.william.bell@gmail.com>
2019-07-17 12:59:02 -0600

    added mongolab url for heroku for connection

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/pub-meb-server-api', {
+const localMongoUrl = 'mongodb://127.0.0.1:27017/pub-meb-server-api';
+const herokuMongoUrl = process.env.MONGODB_URI;
+
+mongoose.connect(herokuMongoUrl || localMongoUrl, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
