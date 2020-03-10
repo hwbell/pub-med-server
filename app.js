b34ce993be845f83972ef888da8c6145cc42e866
Harry <harold.william.bell@gmail.com>
2020-03-09 18:34:08 -0600

    try to fix cors methods

--- app.js
+++ app.js
@@ -23,12 +23,11 @@ app.set('view engine', 'jade');
 // for cors pre-flight requests
 var cors = require('cors');
 app.options('*', cors());
-app.use(cors({ origin: 'https://pub-med-app.herokuapp.com/' , credentials :  true}));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
